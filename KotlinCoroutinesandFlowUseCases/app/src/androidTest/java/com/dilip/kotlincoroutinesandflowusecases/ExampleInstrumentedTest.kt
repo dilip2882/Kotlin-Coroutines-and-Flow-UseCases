525b74c3c4474790651008782a00162fbe9b6f8b
Dilip Patel <pateldilip2902@gmail.com>
2024-09-12 18:19:12 +0530

    initial structure

--- KotlinCoroutinesandFlowUseCases/app/src/androidTest/java/com/dilip/kotlincoroutinesandflowusecases/ExampleInstrumentedTest.kt
+++ KotlinCoroutinesandFlowUseCases/app/src/androidTest/java/com/dilip/kotlincoroutinesandflowusecases/ExampleInstrumentedTest.kt
@@ -1,12 +1,10 @@
-package com.dilip.kotlin_coroutines_and_flow_usecases
+package com.dilip.kotlincoroutinesandflowusecases
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -19,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.dilip.kotlin_coroutines_and_flow_usecases", appContext.packageName)
+        assertEquals("com.lukaslechner.coroutineusecasesonandroid", appContext.packageName)
     }
 }
--- KotlinCoroutinesandFlowUseCases/app/src/test/java/com/dilip/kotlincoroutinesandflowusecases/ExampleUnitTest.kt
+++ KotlinCoroutinesandFlowUseCases/app/src/test/java/com/dilip/kotlincoroutinesandflowusecases/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
-package com.dilip.kotlin_coroutines_and_flow_usecases
+package com.dilip.kotlincoroutinesandflowusecases
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
