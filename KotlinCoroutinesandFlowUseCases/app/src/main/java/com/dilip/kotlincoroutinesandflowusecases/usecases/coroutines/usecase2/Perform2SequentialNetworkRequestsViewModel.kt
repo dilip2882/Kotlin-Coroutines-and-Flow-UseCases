7c523a32699e5c28a519fa89258c6a4c0675a033
Dilip Patel <pateldilip2902@gmail.com>
2024-09-13 10:48:50 +0530

    perform2SequentialNetworkRequest using coroutine

--- KotlinCoroutinesandFlowUseCases/app/src/main/java/com/dilip/kotlincoroutinesandflowusecases/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
+++ KotlinCoroutinesandFlowUseCases/app/src/main/java/com/dilip/kotlincoroutinesandflowusecases/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
@@ -1,13 +1,28 @@
 package com.dilip.kotlincoroutinesandflowusecases.usecases.coroutines.usecase2
 
+import androidx.lifecycle.viewModelScope
 import com.dilip.kotlincoroutinesandflowusecases.base.BaseViewModel
 import com.dilip.kotlincoroutinesandflowusecases.mock.MockApi
+import kotlinx.coroutines.launch
 
 class Perform2SequentialNetworkRequestsViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun perform2SequentialNetworkRequest() {
+        uiState.value = UiState.Loading
+        viewModelScope.launch {
+            try {
+                val recentVersions = mockApi.getRecentAndroidVersions()
+                val mostRecentVersion = recentVersions.last()
 
+                val featuresOfMostRecentVersion =
+                    mockApi.getAndroidVersionFeatures(mostRecentVersion.apiLevel)
+
+                uiState.value = UiState.Success(featuresOfMostRecentVersion)
+            } catch (exception: Exception) {
+                uiState.value = UiState.Error("Network Request failed")
+            }
+        }
     }
 }
