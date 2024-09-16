bf2eb0936a891d7fe66ee1dacdae3406cd8c397d
Dilip Patel <pateldilip2902@gmail.com>
2024-09-16 15:30:47 +0530

    Variable Amount Of Network Requests

--- KotlinCoroutinesandFlowUseCases/app/src/main/java/com/dilip/kotlincoroutinesandflowusecases/usecases/coroutines/usecase4/VariableAmountOfNetworkRequestsViewModel.kt
+++ KotlinCoroutinesandFlowUseCases/app/src/main/java/com/dilip/kotlincoroutinesandflowusecases/usecases/coroutines/usecase4/VariableAmountOfNetworkRequestsViewModel.kt
@@ -1,17 +1,48 @@
 package com.dilip.kotlincoroutinesandflowusecases.usecases.coroutines.usecase4
 
+import androidx.lifecycle.viewModelScope
 import com.dilip.kotlincoroutinesandflowusecases.base.BaseViewModel
 import com.dilip.kotlincoroutinesandflowusecases.mock.MockApi
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.launch
 
 class VariableAmountOfNetworkRequestsViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequestsSequentially() {
+        uiState.value = UiState.Loading
 
+        viewModelScope.launch {
+            try {
+                val recentVersions = mockApi.getRecentAndroidVersions()
+                val versionFeatures = recentVersions.map { androidVersion ->
+                    mockApi.getAndroidVersionFeatures(androidVersion.apiLevel)
+                }
+                uiState.value = UiState.Success(versionFeatures)
+            } catch (e: Exception) {
+                uiState.value = UiState.Error("Network Request failed")
+
+            }
+        }
     }
 
     fun performNetworkRequestsConcurrently() {
+        uiState.value = UiState.Loading
 
+        viewModelScope.launch {
+            try {
+                val recentVersions = mockApi.getRecentAndroidVersions()
+                val versionFeatures = recentVersions.map { androidVersion ->
+                    async {
+                        mockApi.getAndroidVersionFeatures(androidVersion.apiLevel)
+                    }
+                }.awaitAll()
+                uiState.value = UiState.Success(versionFeatures)
+            } catch (e: Exception) {
+                uiState.value = UiState.Error("Network Request failed")
+            }
+        }
     }
 }
